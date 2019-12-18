67feadc66897dc1d7aeec0d7c53efd7df05ad685
360matt <mattbacq2004@gmail.com>
2019-12-18 18:33:49 +0100

    Fix et mise en marche du CommandManager et ajout de quelques entrées dans le fichier de langue

--- UlityCore/src/fr/ulity/core/bukkit/CommandManager.java
+++ UlityCore/src/fr/ulity/core/bukkit/CommandManager.java
@@ -1,6 +1,8 @@
 package fr.ulity.core.bukkit;
 
-import org.bukkit.configuration.ConfigurationSection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import fr.ulity.core.utils.Syntax;
 
@@ -10,15 +12,21 @@ public class CommandManager {
 	String CommandName;
 	String PluginName;
 
+	public CommandManager() {
+		reload();
+	}
+	
 	public CommandManager (String _p, String _n) {
 		if (CommandConf == null)
-			CommandConf = new Config("language/" + Config.lang + "_commands");
+			reload();
 		PluginName = _p;
 		CommandName = _n;
 	}
 	
-	public CommandManager() {
-		reload();
+	public CommandManager(String _p) {
+		if (CommandConf == null)
+			reload();
+		PluginName = _p;
 	}
 	
 	public void reload() {
@@ -26,17 +34,48 @@ public class CommandManager {
 		CommandConf.reload();
 	}
 	
-	public void description (String Description) {
+	public String description (String Description) {
+		if (CommandConf == null)
+			reload();
+		return CommandConf.getString(PluginName + "." + CommandName + ".description", Description);
+	}
+	
+	public String syntax (String ...SyntaxCommand) {
+		if (CommandConf == null)
+			reload();
+		return CommandConf.getString(PluginName + "." + CommandName + ".syntax", Syntax.runSimple(CommandName, SyntaxCommand));
+	}
+	
+	public Map<String, String> getCommand (String Name) {
+		if (CommandConf == null)
+			reload();
 
-		CommandConf.get(PluginName + "." + CommandName + ".description", Description);
+		Map <String, String> hash = new HashMap<String, String>();
+		hash.put("syntax", CommandConf.getString(PluginName + "." + Name + ".syntax"));
+		hash.put("description", CommandConf.getString(PluginName + "." + Name + ".description"));
+		
+		return hash;
 	}
 	
-	public void syntax (String ...SyntaxCommand) {
-		CommandConf.get(PluginName + "." + CommandName + ".syntax", Syntax.runSimple(CommandName, SyntaxCommand));
-	}
-	
-	public ConfigurationSection getCommand (String CommandName) {
-		return CommandConf.getSection(PluginName + "." + CommandName);
+
+	public ArrayList<Map<String, String>> getCommandsFromList (String CommandListName) {
+		if (CommandConf == null)
+			reload();
+		
+		ArrayList<Map <String, String>> cmd = new ArrayList<Map <String, String>>();
+		
+		for (String x: CommandConf.getSection(PluginName + "." + CommandListName).getKeys(false)) {
+			if (!x.equals("syntax") && !x.equals("description")) {
+				Map <String, String> hash = new HashMap<String, String>();
+				hash.put("name", x);
+				hash.put("syntax", CommandConf.getString(PluginName + "." + CommandListName + "." + x + ".syntax"));
+				hash.put("description", CommandConf.getString(PluginName + "." + CommandListName + "." + x + ".description"));
+				
+				cmd.add(hash);
+			}
+		}
+		
+		return cmd;
 	}
 
 	
